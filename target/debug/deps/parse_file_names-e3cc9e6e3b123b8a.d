5983b377e83aa82fcc2b367e764da68554dc8e0f
David <dgreen2@uvm.edu>
2024-05-28 15:26:42 -0400

    yeah

--- target/debug/deps/parse_file_names-e3cc9e6e3b123b8a.d
+++ target/debug/deps/parse_file_names-e3cc9e6e3b123b8a.d
@@ -1,5 +1,5 @@
-/media/david/WorkDrive/Documents/UVM/HarrisLab/Sammi/rust/projects/parse_file_names/target/debug/deps/libparse_file_names-e3cc9e6e3b123b8a.rmeta: src/main.rs
+/home/david/Documents/Academic/UVM/Harris_Lab/Sami Connolly/rust/projects/parse_file_names/target/debug/deps/libparse_file_names-e3cc9e6e3b123b8a.rmeta: src/main.rs
 
-/media/david/WorkDrive/Documents/UVM/HarrisLab/Sammi/rust/projects/parse_file_names/target/debug/deps/parse_file_names-e3cc9e6e3b123b8a.d: src/main.rs
+/home/david/Documents/Academic/UVM/Harris_Lab/Sami Connolly/rust/projects/parse_file_names/target/debug/deps/parse_file_names-e3cc9e6e3b123b8a.d: src/main.rs
 
 src/main.rs:
